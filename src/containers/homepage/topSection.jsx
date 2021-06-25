7c00502b2f35e03943c914d972083c7532b505f0
benGag <ben.gagro@gmail.com>
2021-06-25 12:24:07 +0200

    Add top center logo "Na Stope"

--- src/containers/homepage/topSection.jsx
+++ src/containers/homepage/topSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import BackgroundImg from "../../assets/pictures/background.jpg";
+import { Logo } from "../../components/logo";
 
 const TopContainer = styled.div`
     width: 100%;
@@ -15,12 +16,17 @@ const BackgroundFilter = styled.div`
     width: 100%;
     height: 100%;
     background-image: linear-gradient(360deg, rgba(0, 0, 0, 0.08) 0%, rgba(0, 0, 0, 0) 100%);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
 `;
 
 export function TopSection(props) {
     return <TopContainer>
         <BackgroundFilter>
-            boom
+            <Logo />
         </BackgroundFilter>
     </TopContainer>;
 }
